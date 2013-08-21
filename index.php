4dd868b8f51444979d05c37ca54083bd045e72c0
Ricard <rtorres@coexia.cat>
2013-08-21 09:40:12 +0200

    initial

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Backbone JS - Tests</title>
+	<link href='http://fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
+	<style>
+		html{
+			font-size:10px;
+		}
+		body{
+			font-size:1.6rem;
+			font-family: 'Lato', sans-serif;
+			margin: 5rem;
+		}
+	</style>
+</head>
+<body>
+
+	<header>
+		<nav>
+			<ul>
+				<li>Your menu</li>
+			</ul>
+		</nav>
+	</header>
+	
+	<section>	
+		<article>
+			<header>
+				<h2>Article title</h2>				
+			</header>
+			<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+		</article>
+	</section>
+
+	<aside>
+		<h2>About section</h2>
+		<p>Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+	</aside>
+
+	<footer>
+		<p>This is the Footer</p>
+	</footer>
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>	
+	<script src="http://underscorejs.org/underscore-min.js"></script>	
+	<script src="http://backbonejs.org/backbone-min.js"></script>	
+
+</body>
+</html>
